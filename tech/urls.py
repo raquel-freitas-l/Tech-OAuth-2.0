3c26ee1ae2d05e21495389bf9e0bfe91c5773604
Raquel Freitas <terezaraquel010@gmail.com>
2024-11-05 16:55:47 -0300

    Tech-OAuth-2.0

---
+++ tech/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from tech.views import index, members
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('members/', members, name='members'),
+]
+
---
+++ tech/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+def index(request):
+    return render(request,'index.html',)
+
+@login_required
+def members(request):
+    return render(request,'members.html',)
